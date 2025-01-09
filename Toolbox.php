59dd92c51366e8bb34c18c3aa8cbfb071190d71c
Berk Güçlükol <berkguclukol@gmail.com>
2025-01-09 08:17:24 +0300

    - getItemsInDirectory() function added.

--- Toolbox.php
+++ Toolbox.php
@@ -1,13 +1,12 @@
 <?php
-
-
 class Toolbox
 {
     /**
      * @param $string
      * @param $values
      * @return string
-     * @example createStringWithFormat("There are {0} new members today and {1} yesterday.", array("15", "45")) => There are 15 new members today and 45 yesterday.
+     * @example createStringWithFormat("There are {0} new members today and {1} yesterday.", array("15", "45"))
+     * @result There are 15 new members today and 45 yesterday.
      */
     static function createStringWithFormat($string, $values): string
     {
@@ -21,7 +20,8 @@ class Toolbox
      * @param $startDate
      * @return string
      * @throws Exception
-     * @example Toolbox::createSpecialVersion("2024-01-01") => v1.8
+     * @example Toolbox::createSpecialVersion("2024-01-01")
+     * @result v1.8
      */
     static function createSpecialVersion($startDate): string
     {
@@ -36,5 +36,28 @@ class Toolbox
         $string .= ($days > 1) ? '.'.$days : '';
         return $string;
     }
+
+    /**
+     * @param $directory
+     * @return array
+     * @throws Exception
+     * @example Toolbox::getItemsInDirectory("./")
+     * @result Array ([0] => LICENSE [1] => README.md [2] => Toolbox.php)
+     */
+    static function getItemsInDirectory($directory): array
+    {
+        $returns = [];
+        if ($handle = opendir($directory)) {
+            while (false !== ($file = readdir($handle))) {
+                if ($file[0] != ".") {
+                    $returns[] = $file;
+                }
+            }
+            closedir($handle);
+        }
+        return $returns;
+    }
+
+
 
 }
